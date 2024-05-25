4cee7c6f88194c689f6ed9eff6891c4186337f09
soeguet <osman_s@live.de>
2024-05-25 13:47:50 +0200

    fixed: postcss config to json, added tests

--- frontend/src/components/error/ClientNotFoundPage.test.tsx
+++ frontend/src/components/error/ClientNotFoundPage.test.tsx
@@ -1,16 +1,11 @@
 import React from "react";
-import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import { ClientNotFoundPage } from "./ClientNotFoundPage";
 
 describe("ClientNotFoundPage", () => {
     beforeEach(() => {
         render(<ClientNotFoundPage />);
     });
-
-    // test("renders client not found message", () => {
-    //     const clientNotFoundMessage = screen.getByText("Client not found");
-    //     expect(clientNotFoundMessage).toBeInTheDocument();
-    // });
 
     test("renders reconnect button", () => {
         const reconnectButton = screen.getByText("Reconnect");
@@ -39,25 +34,16 @@ describe("ClientNotFoundPage", () => {
         }
         expect(button).toBeDisabled();
     });
-    test.skip("reconnect button is enabled after 2000ms", async () => {
-        const button = screen.queryByTestId(
+
+    test("reconnect button is enabled after 2000ms", async () => {
+        const button = await screen.findByTestId(
             "reconnect-button-client-not-found"
         );
-        await waitFor(() => {}, { timeout: 5000 });
-
-        if (button === null) {
-            throw new Error("Button should not be null");
-        }
-        expect(button).not.toBeDisabled();
+        await waitFor(
+            () => {
+                expect(button).not.toBeDisabled();
+            },
+            { timeout: 2000 }
+        );
     });
-
-    // test("clicking on reconnect button calls WindowReloadApp", () => {
-    //     const mockWindowReloadApp = vi.fn();
-    //     vi.spyOn(global, "WindowReloadApp").mockImplementation(
-    //         mockWindowReloadApp
-    //     );
-    //     const reconnectButton = screen.getByText("Reconnect");
-    //     fireEvent.click(reconnectButton);
-    //     expect(mockWindowReloadApp).toHaveBeenCalled();
-    // });
 });
