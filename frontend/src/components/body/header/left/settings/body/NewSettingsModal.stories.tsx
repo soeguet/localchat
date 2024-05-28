ca0e2649001d9ff905a00bf27772ef75c501cce5
soeguet <osman_s@live.de>
2024-05-28 13:43:27 +0200

    added: storybook

---
+++ frontend/src/components/body/header/left/settings/body/NewSettingsModal.stories.tsx
@@ -0,0 +1,12 @@
+import { NewSettingsModal } from "./NewSettingsModal";
+
+export default {
+    component: NewSettingsModal,
+};
+
+export const Primary = {
+    args: {
+        label: "Settings Modal",
+        primary: true,
+    },
+};
---
+++ frontend/src/components/body/header/left/settings/middle/NewInputIp.stories.tsx
@@ -0,0 +1,12 @@
+import { NewInputIp } from "./NewInputIp";
+
+export default {
+    component: NewInputIp,
+};
+
+export const Primary = {
+    args: {
+        label: "IP Address",
+        primary: true,
+    },
+};
