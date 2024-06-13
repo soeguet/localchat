dd2edf19dd66af25671da645c69a3f152430f45b
soeguet <osman_s@live.de>
2024-06-13 12:27:16 +0200

    fix: added additional check for pictures, enable sending empty text if picture is available

--- frontend/src/components/body/input/ChatInputSection.tsx
+++ frontend/src/components/body/input/ChatInputSection.tsx
@@ -9,89 +9,91 @@ import { Reply } from "./Reply";
 import { Image } from "./Image";
 import { SendButton } from "./SendButton";
 import { TextArea } from "./TextArea";
+import { useImageStore } from "../../../stores/imageStore";
 
 function ChatInputSection() {
-    const { typingTimeoutId, setTypingTimeoutId, sendTypingStatus } =
-        useTypingHook();
-    const [message, setMessage] = useState("");
+	const { typingTimeoutId, setTypingTimeoutId, sendTypingStatus } =
+		useTypingHook();
+	const [message, setMessage] = useState("");
 
-    const handleSendMessage = useCallback(async () => {
-        if (message.trim().length === 0) {
-            return;
-        }
-        if (message) {
-            const { replyMessage, setReplyMessage } = useReplyStore.getState();
-            await sendClientMessageToWebsocket(message);
+	const handleSendMessage = useCallback(async () => {
+		const imageSet = useImageStore.getState().selectedImage;
+		if (!imageSet && message.trim().length === 0) {
+			return;
+		}
+		if (message || imageSet) {
+			const { replyMessage, setReplyMessage } = useReplyStore.getState();
+			await sendClientMessageToWebsocket(message);
 
-            // reset replyMessage state AFTER sending the message. we need that state for the message payload
-            if (replyMessage !== null) {
-                setReplyMessage(null);
-            }
-            setMessage("");
-        }
-    }, [message]);
+			// reset replyMessage state AFTER sending the message. we need that state for the message payload
+			if (replyMessage !== null) {
+				setReplyMessage(null);
+			}
+			setMessage("");
+		}
+	}, [message]);
 
-    const handleKeyDown = useCallback(
-        (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
-            if (e.key === "Enter" && !e.shiftKey) {
-                e.preventDefault();
-                handleSendMessage();
-                sendTypingStatus(false);
-                if (typingTimeoutId) {
-                    clearTimeout(typingTimeoutId);
-                    setTypingTimeoutId(null);
-                }
+	const handleKeyDown = useCallback(
+		(e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+			if (e.key === "Enter" && !e.shiftKey) {
+				e.preventDefault();
+				handleSendMessage();
+				sendTypingStatus(false);
+				if (typingTimeoutId) {
+					clearTimeout(typingTimeoutId);
+					setTypingTimeoutId(null);
+				}
 
-                setMessage("");
-            } else {
-                // sends status
-                if (message.length === 0 || !typingTimeoutId) {
-                    sendTypingStatus(true);
-                }
+				setMessage("");
+			} else {
+				// sends status
+				if (message.length === 0 || !typingTimeoutId) {
+					sendTypingStatus(true);
+				}
 
-                // reset timer if one is present
-                if (typingTimeoutId) {
-                    clearTimeout(typingTimeoutId);
-                }
+				// reset timer if one is present
+				if (typingTimeoutId) {
+					clearTimeout(typingTimeoutId);
+				}
 
-                // start a new timer
-                const id = window.setTimeout(() => {
-                    sendTypingStatus(false);
-                    setTypingTimeoutId(null);
-                }, 2500);
-                setTypingTimeoutId(id);
-            }
-        },
-        [
-            typingTimeoutId,
-            message,
-            setTypingTimeoutId,
-            sendTypingStatus,
-            handleSendMessage,
-        ]
-    );
+				// start a new timer
+				const id = window.setTimeout(() => {
+					sendTypingStatus(false);
+					setTypingTimeoutId(null);
+				}, 2500);
+				setTypingTimeoutId(id);
+			}
+		},
+		[
+			typingTimeoutId,
+			message,
+			setTypingTimeoutId,
+			sendTypingStatus,
+			handleSendMessage,
+		]
+	);
 
-    return (
-        <>
-            <div className="flex grow-0 items-end gap-2 border-t-2 border-t-black bg-white p-4">
-                <Emoji setMessage={setMessage} />
-                <ClipButton />
-                <div className="mx-2 my-auto flex flex-1 flex-col gap-2">
-                    <Reply />
-                    <Image />
-                    <TextArea
-                        message={message}
-                        setMessage={setMessage}
-                        handleKeyDown={handleKeyDown}
-                    />
-                </div>
-                <SendButton
-                    handleSendMessage={handleSendMessage}
-                    sendTypingStatus={sendTypingStatus}
-                />
-            </div>
-        </>
-    );
+	return (
+		<>
+			<div className="flex grow-0 items-end gap-2 border-t-2 border-t-black bg-white p-4">
+				<Emoji setMessage={setMessage} />
+				<ClipButton />
+				<div className="mx-2 my-auto flex flex-1 flex-col gap-2">
+					<Reply />
+					<Image />
+					<TextArea
+						message={message}
+						setMessage={setMessage}
+						handleKeyDown={handleKeyDown}
+					/>
+				</div>
+				<SendButton
+					handleSendMessage={handleSendMessage}
+					sendTypingStatus={sendTypingStatus}
+				/>
+			</div>
+		</>
+	);
 }
 
 export { ChatInputSection };
