e76c67343db736bfd263cea00a877635785dd941
soeguet <osman_s@live.de>
2024-06-23 11:54:11 +0200

    add: messages of client should be on the right hand side

--- frontend/src/components/body/emergency/EmergencyChat.tsx
+++ frontend/src/components/body/emergency/EmergencyChat.tsx
@@ -4,9 +4,12 @@ import { useEmergencyStore } from "../../../stores/emergencyStore";
 import { useUserStore } from "../../../stores/userStore";
 import { base64ToUtf8 } from "../../../utils/encoder";
 import { HelpCallerSvg } from "../../svgs/emergency/HelpCallerSvg";
+import { useEffect, useRef } from "react";
+import type { EmergencyMessage } from "../../../utils/customTypes";
 
 function EmergencyChat() {
 	const { t } = useTranslation();
+	const chatContainerRef = useRef<HTMLDivElement>(null);
 	const emergencyMessages = useEmergencyStore(
 		(state) => state.emergencyMessages,
 	);
@@ -15,16 +18,104 @@ function EmergencyChat() {
 	const emergencyInitiatorId = useEmergencyStore(
 		(state) => state.emergencyInitiatorId,
 	);
+	const emergencyInitiatorColor = useClientStore(
+		(state) =>
+			state.clients.find(
+				(client) => client.clientDbId === emergencyInitiatorId,
+			)?.clientColor,
+	);
 	const emergencyInitiatorName = useClientStore(
 		(state) =>
 			state.clients.find(
 				(client) => client.clientDbId === emergencyInitiatorId,
 			)?.clientUsername,
 	);
+
+	// biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+	useEffect(() => {
+		const container = chatContainerRef.current;
+		if (container === null) {
+			return;
+		}
+		container.scrollTop = container.scrollHeight;
+	}, [emergencyMessages]);
+
+	function messageTemplate(
+		messagFromThisClient: boolean,
+		message: EmergencyMessage,
+		messageSenderColor: string,
+		decodedMessage: string,
+		clientUsername: string,
+	) {
+		if (!messagFromThisClient) {
+			return (
+				<>
+					<div
+						className="flex gap-3 font-semibold"
+						style={{
+							color: messageSenderColor,
+						}}>
+						{emergencyInitiatorId === message.clientDbId && (
+							<HelpCallerSvg />
+						)}
+						{clientUsername}
+					</div>
+					<div className="flex">
+						<div className="ml-3 flex items-center text-xl text-gray-400">
+							{message.time}
+						</div>
+						<div
+							className="ml-5 border-l-2 pl-3"
+							style={{
+								borderColor: messageSenderColor,
+							}}>
+							{decodedMessage}
+						</div>
+					</div>
+				</>
+			);
+		}
+
+		return (
+			<>
+				<div
+					className="flex justify-end gap-3 font-semibold"
+					style={{
+						color: messageSenderColor,
+					}}>
+					{emergencyInitiatorId === message.clientDbId && (
+						<HelpCallerSvg />
+					)}
+					{clientUsername}
+				</div>
+				<div className="flex justify-end">
+					<div
+						className="mr-5 border-r-2 pr-3"
+						style={{
+							borderColor: messageSenderColor,
+						}}>
+						{decodedMessage}
+					</div>
+					<div className="mr-3 flex items-center text-xl text-gray-400">
+						{message.time}
+					</div>
+				</div>
+			</>
+		);
+	}
+
 	return (
 		<>
-			<div className="h-full w-full grow">
-				<div className="m-5 animate-bounce rounded-2xl bg-red-300/50 p-2 text-center">
+			<div
+				ref={chatContainerRef}
+				className="w-full overflow-y-scroll border-0 px-3">
+				<div
+					className="m-1 mt-3 animate-bounce rounded-2xl border-0 p-2 text-center"
+					style={{
+						backgroundColor: emergencyInitiatorColor,
+						opacity: 0.8,
+						color: "white",
+					}}>
 					{t("emergency_chat_init_text", {
 						initiatorName: emergencyInitiatorName,
 					})}
@@ -37,29 +128,35 @@ function EmergencyChat() {
 					if (messageSender === undefined || messageSender === null) {
 						throw new Error("messageSender is undefined");
 					}
-					const messageSenderColor = messageSender.clientColor;
+					const messageSenderColor = messageSender.clientColor
+						? messageSender.clientColor
+						: "red";
+					const messagFromThisClient =
+						message.clientDbId === myClientId;
 
 					return (
 						<div
 							key={message.messageDbId}
 							className="flex flex-col gap-1 p-2">
-							<div
-								className="flex gap-3 font-semibold"
-								style={{
-									color: messageSenderColor,
-								}}>
-								{myClientId === message.clientDbId && (
-									<HelpCallerSvg />
-								)}
-								{messageSender.clientUsername}
-							</div>
-							<div
-								className="ml-12 border-l-2 pl-2"
-								style={{
-									borderColor: messageSenderColor,
-								}}>
-								{decodedMessage}
-							</div>
+							{messageTemplate(
+								messagFromThisClient,
+								message,
+								messageSenderColor,
+								decodedMessage,
+								messageSender.clientUsername,
+							)}
+							{/* <div className="flex"> */}
+							{/* 	<div className="ml-3 flex items-center text-xl text-gray-400"> */}
+							{/* 		{message.time} */}
+							{/* 	</div> */}
+							{/* 	<div */}
+							{/* 		className="ml-5 border-l-2 pl-3" */}
+							{/* 		style={{ */}
+							{/* 			borderColor: messageSenderColor, */}
+							{/* 		}}> */}
+							{/* 		{decodedMessage} */}
+							{/* 	</div> */}
+							{/* </div> */}
 						</div>
 					);
 				})}
