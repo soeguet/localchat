b42ee758b5dbac38db631ec7a56b7ab62a41e393
soeguet <osman_s@live.de>
2024-07-12 22:37:35 +0200

    add license + WIP chat banner support

--- frontend/src/components/body/panel/ChatPanel.tsx
+++ frontend/src/components/body/panel/ChatPanel.tsx
@@ -1,11 +1,14 @@
 import { useEffect, useRef } from "react";
 import { MessageRenderMap } from "./MessageRenderMap";
 import { useRefStore } from "../../../stores/refStore";
+import { TopBanner } from "./banner/TopBanner";
 
 function ChatPanel() {
 	const chatBottomRef = useRef<HTMLDivElement>(null);
 	const chatContainerRef = useRef<HTMLDivElement>(null);
-	const setChatContainerRef = useRefStore((state) => state.setChatContainerRef);
+	const setChatContainerRef = useRefStore(
+		(state) => state.setChatContainerRef,
+	);
 	const setChatBottomRef = useRefStore((state) => state.setChatBottomRef);
 
 	// biome-ignore lint/correctness/useExhaustiveDependencies: only needed once
@@ -16,10 +19,10 @@ function ChatPanel() {
 
 	return (
 		<>
+			<TopBanner />
 			<div
 				ref={chatContainerRef}
-				className={"relative grow overflow-y-auto px-5 pb-2 pt-2"}
-			>
+				className={"relative grow overflow-y-auto px-5 pb-2 pt-2"}>
 				<MessageRenderMap />
 				<div ref={chatBottomRef} className="h-2 bg-transparent" />
 			</div>
