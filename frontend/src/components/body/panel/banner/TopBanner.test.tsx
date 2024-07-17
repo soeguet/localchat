3bf957d4c88e82f61064f9e124d31ba33197d455
soeguet <osman_s@live.de>
2024-07-17 22:13:35 +0200

    test: top banner visibility & hidden state

---
+++ frontend/src/components/body/panel/banner/TopBanner.test.tsx
@@ -0,0 +1,60 @@
+import { expect, test, describe } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { useBannerStore } from "../../../../stores/bannerStore";
+import type { BannerObject } from "../../../../utils/customTypes";
+import { TopBanner } from "./TopBanner";
+
+describe("TopBanner", () => {
+	test("render banner list", async () => {
+		const banners: BannerObject[] = [
+			{
+				id: "1",
+				title: "localchat v1",
+				message: "join our local chat!",
+				priority: 1,
+				hidden: false,
+			},
+			{
+				id: "2",
+				title: "localchat v2",
+				message: "join our prio 2 local chat!",
+				priority: 2,
+				hidden: false,
+			},
+		];
+		useBannerStore.getState().setBanners(banners);
+
+		render(<TopBanner />);
+
+		const bannerListContainer = await screen.findByTestId(
+			"banner-list-container",
+		);
+		expect(bannerListContainer).toBeInTheDocument();
+	});
+	test("dont render banner list, if all banners are hidden", () => {
+		const banners: BannerObject[] = [
+			{
+				id: "1",
+				title: "localchat v1",
+				message: "join our local chat!",
+				priority: 1,
+				hidden: true,
+			},
+			{
+				id: "2",
+				title: "localchat v2",
+				message: "join our prio 2 local chat!",
+				priority: 2,
+				hidden: true,
+			},
+		];
+		useBannerStore.getState().setBanners(banners);
+
+		render(<TopBanner />);
+
+		const bannerListContainer = screen.queryByTestId(
+			"banner-list-container",
+		);
+		expect(bannerListContainer).toBeNull();
+	});
+});
