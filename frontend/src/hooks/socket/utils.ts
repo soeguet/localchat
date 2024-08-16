f7f540bcd8c87eab143c3b3c3e93ffb608d2c96f
soeguet <osman_s@live.de>
2024-08-16 18:22:00 +0200

    add: ClientList Validation

--- frontend/src/hooks/socket/utils.ts
+++ frontend/src/hooks/socket/utils.ts
@@ -1,161 +1,161 @@
-import {
-  MakeWindowsTaskIconFlash,
-  Notification,
-} from "../../../wailsjs/go/main/App";
-import { WindowIsMinimised, WindowShow } from "../../../wailsjs/runtime";
-import { useRefStore } from "../../stores/refStore";
-import { useDoNotDisturbStore } from "../../stores/doNotDisturbStore";
-import { useUnseenMessageCountStore } from "../../stores/unseenMessageCountStore";
-import { useUserStore } from "../../stores/userStore";
+import {MakeWindowsTaskIconFlash, Notification,} from "../../../wailsjs/go/main/App";
+import {WindowIsMinimised, WindowShow} from "../../../wailsjs/runtime";
+import {useRefStore} from "../../stores/refStore";
+import {useDoNotDisturbStore} from "../../stores/doNotDisturbStore";
+import {useUnseenMessageCountStore} from "../../stores/unseenMessageCountStore";
+import {useUserStore} from "../../stores/userStore";
 import {
   ClientEntity,
   ClientListPayload,
-  ClientListPayloadEnhanced, MessageListPayload,
+  ClientListPayloadEnhanced,
+  ClientListPayloadEnhancedSchema,
+  MessageListPayload,
   MessagePayload,
-  PayloadSubType, PayloadSubTypeEnum,
 } from "../../utils/customTypes";
-import { useMessageMapStore } from "../../stores/messageMapStore";
-import { useClientStore } from "../../stores/clientStore";
-import { useGuiHasFocusStore } from "../../stores/guiHasFocusStore";
-import { scrollToBottom } from "../../utils/functionality";
-import { base64ToUtf8 } from "../../utils/encoder";
-import { useVersionStore } from "../../stores/versionStore";
+import {useMessageMapStore} from "../../stores/messageMapStore";
+import {useClientStore} from "../../stores/clientStore";
+import {useGuiHasFocusStore} from "../../stores/guiHasFocusStore";
+import {scrollToBottom} from "../../utils/functionality";
+import {base64ToUtf8} from "../../utils/encoder";
+import {useVersionStore} from "../../stores/versionStore";
+import {errorLogger} from "../../logger/errorLogger";
 
 export function checkIfMessageIsToBeAddedToTheUnseenMessagesList(
-  messagePayload: MessagePayload,
-  addIdToList: boolean,
+    messagePayload: MessagePayload,
+    addIdToList: boolean,
 ) {
-  // if we don't need to scroll to the bottom, we need to add the message to the unseen messages list
+    // if we don't need to scroll to the bottom, we need to add the message to the unseen messages list
 
-  if (addIdToList) {
-    useUnseenMessageCountStore
-      .getState()
-      .addMessageToUnseenMessagesList(messagePayload.messageType.messageDbId);
-  } else {
-    useUnseenMessageCountStore.getState().resetUnseenMessageCount();
-    scrollToBottom();
-  }
+    if (addIdToList) {
+        useUnseenMessageCountStore
+            .getState()
+            .addMessageToUnseenMessagesList(messagePayload.messageType.messageDbId);
+    } else {
+        useUnseenMessageCountStore.getState().resetUnseenMessageCount();
+        scrollToBottom();
+    }
 }
 
 export function checkIfNotificationIsNeeded(messagePayload: MessagePayload) {
-  // no message allowed if "do not disturb" is active
-  if (useDoNotDisturbStore.getState().doNotDisturb) {
-    return;
-  }
-  // no message needed if message from this client
-  if (messagePayload.clientType.clientDbId === useUserStore.getState().myId) {
-    return;
-  }
-
-  // first check if gui is even in focus
-  if (useGuiHasFocusStore.getState().guiHasFocus) {
-    // no message needed if already at chat bottom
-    if (!useRefStore.getState().chatBottomRefVisible) {
-      return;
+    // no message allowed if "do not disturb" is active
+    if (useDoNotDisturbStore.getState().doNotDisturb) {
+        return;
     }
-  }
+    // no message needed if message from this client
+    if (messagePayload.clientType.clientDbId === useUserStore.getState().myId) {
+        return;
+    }
 
-  const messageSenderName = useClientStore
-    .getState()
-    .clients.find(
-      (predicate) =>
-        predicate.clientDbId === messagePayload.clientType.clientDbId,
-    )?.clientUsername;
+    // first check if gui is even in focus
+    if (useGuiHasFocusStore.getState().guiHasFocus) {
+        // no message needed if already at chat bottom
+        if (!useRefStore.getState().chatBottomRefVisible) {
+            return;
+        }
+    }
 
-  const titleNotification = `${messagePayload.messageType.messageTime.slice(0, 5)} - ${messageSenderName}`;
+    const messageSenderName = useClientStore
+        .getState()
+        .clients.find(
+            (predicate) =>
+                predicate.clientDbId === messagePayload.clientType.clientDbId,
+        )?.clientUsername;
 
-  // WindowShow();
-  // MakeWindowsTaskIconFlash("localchat");
-  WindowIsMinimised()
-    .then((isMinimised) => {
-      if (isMinimised) {
-        MakeWindowsTaskIconFlash("Localchat");
-        // setTimeout(() => {
-        //     WindowUnminimise();
-        // }, 100);
-        // setTimeout(() => {
-        //     WindowHide();
-        // }, 600);
-      } else {
-        WindowShow();
-      }
-    })
-    .then(async () => {
-      const message = base64ToUtf8(messagePayload.messageType.messageContext);
-      await Notification(titleNotification, message);
-    });
+    const titleNotification = `${messagePayload.messageType.messageTime.slice(0, 5)} - ${messageSenderName}`;
 
-  // WindowIsMinimised().then((isMinimised) => {
-  //     if (isMinimised) {
-  //         MakeWindowsTaskIconFlash("localchat");
-  //     } else {
-  //         WindowShow();
-  //     }
-  // });
+    // WindowShow();
+    // MakeWindowsTaskIconFlash("localchat");
+    WindowIsMinimised()
+        .then((isMinimised) => {
+            if (isMinimised) {
+                MakeWindowsTaskIconFlash("Localchat");
+                // setTimeout(() => {
+                //     WindowUnminimise();
+                // }, 100);
+                // setTimeout(() => {
+                //     WindowHide();
+                // }, 600);
+            } else {
+                WindowShow();
+            }
+        })
+        .then(async () => {
+            const message = base64ToUtf8(messagePayload.messageType.messageContext);
+            await Notification(titleNotification, message);
+        });
+
+    // WindowIsMinimised().then((isMinimised) => {
+    //     if (isMinimised) {
+    //         MakeWindowsTaskIconFlash("localchat");
+    //     } else {
+    //         WindowShow();
+    //     }
+    // });
 }
 
 // updates all clients and caches array of clients
 // uses clientStore
 export function handleClientListPayload(payloadAsString: string) {
-  let payloadAsObject: ClientListPayloadEnhanced;
-  try {
-    payloadAsObject = JSON.parse(payloadAsString);
-  } catch (e) {
-    console.error("Failed to parse client list payload");
-    throw new Error("Failed to parse client list payload");
-  }
+    let payloadAsObject: ClientListPayloadEnhanced;
+    try {
+        payloadAsObject = JSON.parse(payloadAsString);
+    } catch (e) {
+        console.error("Failed to parse client list payload");
+        throw new Error("Failed to parse client list payload");
+    }
 
-  // update info about the newest client version
-  const newVersion = payloadAsObject.version;
-  if (newVersion === undefined) {
-    console.error("Version is undefined");
-    throw new Error("Version is undefined");
-  } else {
-    useVersionStore.getState().checkForUpdate(newVersion);
-  }
+    const clientListValidation = ClientListPayloadEnhancedSchema.safeParse(payloadAsObject);
 
-  const clients: ClientEntity[] = payloadAsObject.clients;
-  useClientStore.getState().setClients(clients);
+    if (clientListValidation.success) {
+        const newVersion = clientListValidation.data.version;
+        useVersionStore.getState().checkForUpdate(newVersion);
+
+        useClientStore.getState().setClients(clientListValidation.data.clients);
+    } else {
+        console.error("Failed to parse client list payload");
+        errorLogger.logError(new Error("Failed to parse client list payload"));
+        throw new Error("Failed to parse client list payload");
+    }
 }
 
 // updates this specific client and caches its values
 // uses userStore
 export function updateThisClientsCachedDataWithNewPayloadData(
-  payloadAsString: string,
+    payloadAsString: string,
 ) {
-  const payloadAsObject: ClientListPayload = JSON.parse(payloadAsString);
-  const clients: ClientEntity[] = payloadAsObject.clients;
-  const myId = useUserStore.getState().myId;
-  const myClient = clients.find((client) => client.clientDbId === myId);
+    const payloadAsObject: ClientListPayload = JSON.parse(payloadAsString);
+    const clients: ClientEntity[] = payloadAsObject.clients;
+    const myId = useUserStore.getState().myId;
+    const myClient = clients.find((client) => client.clientDbId === myId);
 
-  if (myClient === undefined) {
-    throw new Error("Client not found in client list");
-  }
+    if (myClient === undefined) {
+        throw new Error("Client not found in client list");
+    }
 
-  useUserStore.getState().setMyUsername(myClient.clientUsername);
-  useUserStore.getState().setAvailability(myClient.availability);
+    useUserStore.getState().setMyUsername(myClient.clientUsername);
+    useUserStore.getState().setAvailability(myClient.availability);
 
-  if (myClient.clientColor) {
-    useUserStore.getState().setMyColor(myClient.clientColor);
-  }
+    if (myClient.clientColor) {
+        useUserStore.getState().setMyColor(myClient.clientColor);
+    }
 
-  if (myClient.clientProfileImage) {
-    useUserStore.getState().setMyProfilePhoto(myClient.clientProfileImage);
-  }
+    if (myClient.clientProfileImage) {
+        useUserStore.getState().setMyProfilePhoto(myClient.clientProfileImage);
+    }
 }
 
 export function handeMessageListPayload(data: string) {
-  const messageListPayload: MessageListPayload = JSON.parse(data)
+    const messageListPayload: MessageListPayload = JSON.parse(data)
 
-  if (
-    messageListPayload.messageList === undefined ||
-    messageListPayload.messageList === null
-  ) {
-    throw new Error("messageListPayload.payload is empty");
-  }
+    if (
+        messageListPayload.messageList === undefined ||
+        messageListPayload.messageList === null
+    ) {
+        throw new Error("messageListPayload.payload is empty");
+    }
 
-  // biome-ignore lint/complexity/noForEach: <explanation>
-  messageListPayload.messageList.forEach((messagePayload) => {
-    useMessageMapStore.getState().onMessage(messagePayload);
-  });
+    // biome-ignore lint/complexity/noForEach: <explanation>
+    messageListPayload.messageList.forEach((messagePayload) => {
+        useMessageMapStore.getState().onMessage(messagePayload);
+    });
 }
